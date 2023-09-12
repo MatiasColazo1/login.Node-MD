81f2d06e700b82d349bbac7935f85b19fe560da1
matias <maty_colazo@hotmail.com>
2023-09-11 23:07:57 -0300

    arreglo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+
+// Rutas
+app.use('/', require('./routes/index'));
 
 // starting the server
 app.listen(app.get('port'),() => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-
+    res.render('index');
 });
 
 router.get('/signup', (req, res, next) => {
-
+    res.render('signup');
 });
 
 router.get('/signin', (req, res, next) => {
@@ -20,8 +20,6 @@ router.post('/signup', (req, res, next) => {
 router.post('/signin', (req, res, next) => {
 
 });
-
-
 
 
 module.exports = router
